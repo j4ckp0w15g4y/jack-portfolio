456a9b87c64533bac499c5e025e6258db1aba6f6
Jack Gay <jackpowis@gmail.com>
2019-06-16 13:58:18 -0400

    added components

--- jack-react-portfolio/src/App.js
+++ jack-react-portfolio/src/App.js
@@ -7,15 +7,13 @@ function App() {
   return (
     <div className="App">
       <header>
+
         </header>     
 
         <main>
 
         </main>
-
-        <footer>
-          
-        </footer>
+       
     </div>
   );
 }
