590eede7eaaf8f43928f823179fb02857d471091
Jack Gay <jackpowis@gmail.com>
2019-06-16 13:48:56 -0400

    commit: working on overall app structure

---
+++ jack-react-portfolio/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import { Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        </header>     
+
+        <main>
+
+        </main>
+
+        <footer>
+          
+        </footer>
+    </div>
+  );
+}
+
+export default App;
