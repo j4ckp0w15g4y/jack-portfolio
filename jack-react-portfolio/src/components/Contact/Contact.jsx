7fad3fc94e106323e33ef2ac85969492c1e055e2
Jack Gay <jackpowis@gmail.com>
2019-06-16 15:29:38 -0400

    styling portfolio page

--- jack-react-portfolio/src/components/Contact/Contact.jsx
+++ jack-react-portfolio/src/components/Contact/Contact.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import './Contact.css'
 
 class Contact extends Component {
     render() {
         return (
             <div>
                 <h2>CONTACT</h2>
-                <a href="https://www.linkedin.com/in/jack-gay/" target="_blank" rel="noopener noreferrer">LINKEDIN</a>
-                <a href="mailto:jackpowis@gmail.com" target="_blank" rel="noopener noreferrer">EMAIL ME</a>
+                <a className="contact-link" href="https://www.linkedin.com/in/jack-gay/" target="_blank" rel="noopener noreferrer">LINKEDIN</a>
+                <a className="contact-link" href="mailto:jackpowis@gmail.com" target="_blank" rel="noopener noreferrer">EMAIL ME</a>
                 
             </div>
         );
--- jack-react-portfolio/src/components/Portfolio/Portfolio.jsx
+++ jack-react-portfolio/src/components/Portfolio/Portfolio.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+import './Portfolio.css'
+import Made2FitHero from "../../Assets/made2fit-hero.png"
+import NYCShooting from "../../Assets/nyc-shooting-incident-hero.png"
+import RecycloHero from "../../Assets/recyclo-hero.png"
+
 
 class Portfolio extends Component {
     render() {
         return (
             <div>
+                <div className="portfolio-images">
+                   <a href="https://epic-jones-4619fa.netlify.com/"> <img src={Made2FitHero} className=""></img></a>        
+                   <a href="https://nypd-shooting-incidents-2018.herokuapp.com/"> <img src={NYCShooting} className=""></img></a>        
+                   <a href="http://recyclo.surge.sh/"> <img src={RecycloHero} className=""></img></a>                       
+                </div>
 
-{/* https://epic-jones-4619fa.netlify.com/ */}
-{/* MADE2FIT ^^ */}
-
-{/* https://nypd-shooting-incidents-2018.herokuapp.com/ */}
-{/* Gun Crime App^^ */}
-
-{/* http://recyclo.surge.sh/ */}
-{/* RECYCLO^^ */}
-                
             </div>
         );
     }
