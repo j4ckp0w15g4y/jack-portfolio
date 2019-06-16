457eeccf726408f44d3ed260b14a29dec3054398
Jack Gay <jackpowis@gmail.com>
2019-06-16 15:03:56 -0400

    added portfolio assets

--- jack-react-portfolio/src/components/Home/Home.jsx
+++ jack-react-portfolio/src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h1></h1>
+                <h1>My Website</h1>
             </div>
         );
     }
--- jack-react-portfolio/src/components/Portfolio/Portfolio.jsx
+++ jack-react-portfolio/src/components/Portfolio/Portfolio.jsx
@@ -4,6 +4,15 @@ class Portfolio extends Component {
     render() {
         return (
             <div>
+
+{/* https://epic-jones-4619fa.netlify.com/ */}
+{/* MADE2FIT ^^ */}
+
+{/* https://nypd-shooting-incidents-2018.herokuapp.com/ */}
+{/* Gun Crime App^^ */}
+
+{/* http://recyclo.surge.sh/ */}
+{/* RECYCLO^^ */}
                 
             </div>
         );
