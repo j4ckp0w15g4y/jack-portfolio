ec4dfbc1ba95e4cb1be9ad45a7bebcd95d6a9b31
Jack Gay <jackpowis@gmail.com>
2019-06-17 11:53:41 -0400

    stylistic changes

--- jack-react-portfolio/src/components/Portfolio/Portfolio.jsx
+++ jack-react-portfolio/src/components/Portfolio/Portfolio.jsx
@@ -12,7 +12,7 @@ class Portfolio extends Component {
             <div>
                 <h2>PORTFOLIO</h2>
                 <div className="portfolio-images">
-                   <a href="https://epic-jones-4619fa.netlify.com/" target="_blank" rel="noopener noreferrer"> <img src={Made2FitHero} className="project-image"></img></a>
+                   <a href="https://epic-jones-4619fa.netlify.com/" target="_blank" rel="noopener noreferrer"> <img src={Made2FitHero} className="project-image" title="Made2Fit"></img></a>
                    {/* <p className="project-info">Made2Fit</p> */}
 
                    <a href="https://nypd-shooting-incidents-2018.herokuapp.com/" target="_blank" rel="noopener noreferrer"> <img src={NYCShooting} className="project-image"></img></a>    
