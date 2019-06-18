9f9c1bd29a3647171eb11042170b69add44ad954
Jack Gay <jackpowis@gmail.com>
2019-06-18 18:29:09 -0400

    ffff

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -14,27 +14,33 @@ class Portfolio extends Component {
             <div>
                 <h2>PORTFOLIO</h2>
                 <div className="portfolio-images">
+                    <div className="grid">
                    <a href="https://epic-jones-4619fa.netlify.com/" target="_blank" rel="noopener noreferrer"> <img src={Made2FitHero} className="project-image" title="Made2Fit"></img></a>
                    {/* <p className="project-info">Made2Fit</p> */}
+                   <a href="https://github.com/shenae/made2fit" target="_blank" rel="noopener noreferrer"> <img src={GitHub} id="github"></img></a>        
+                   </div>
 
                    <a href="https://nypd-shooting-incidents-2018.herokuapp.com/" target="_blank" rel="noopener noreferrer"> <img src={NYCShooting} className="project-image"></img></a>    
                    {/* <p className="project-info">NYC Shooting Incidents 2018</p>                        */}
+                   <a href="https://github.com/j4ckp0w15g4y/gun-crime-2018" target="_blank" rel="noopener noreferrer"> <img src={GitHub} id="github"></img></a>  
 
 
                    <a href="http://recyclo.surge.sh/" target="_blank" rel="noopener noreferrer"> <img src={RecycloHero} className="project-image"></img></a>
                    {/* <p className="project-info">Recyclo</p>                        */}
+                   <a href="https://github.com/j4ckp0w15g4y/recyclo" target="_blank" rel="noopener noreferrer"> <img src={GitHub} id="github"></img></a> 
+
+
                    <a href="https://jackgay.herokuapp.com/" target="_blank" rel="noopener noreferrer"> <img src={JackSite} className="project-image"></img></a>
+                   <a href="https://github.com/j4ckp0w15g4y/jack-portfolio" target="_blank" rel="noopener noreferrer"> <img src={GitHub} id="github"></img></a>                        
+
+
                    <a href="https://showhost-nyc.herokuapp.com/" target="_blank" rel="noopener noreferrer"> <img src={ShowHost} className="project-image"></img></a>
+                   <a href="https://github.com/j4ckp0w15g4y/show-host" target="_blank" rel="noopener noreferrer"> <img src={GitHub} id="github"></img></a>                        
 
                 </div>
-                <div className="git-link">
-                <a href="https://github.com/shenae/made2fit" target="_blank" rel="noopener noreferrer"> <img src={GitHub} className=""></img></a>        
-                <a href="https://github.com/j4ckp0w15g4y/gun-crime-2018" target="_blank" rel="noopener noreferrer"> <img src={GitHub} className=""></img></a>  
-                <a href="https://github.com/j4ckp0w15g4y/recyclo" target="_blank" rel="noopener noreferrer"> <img src={GitHub} className=""></img></a> 
-                <a href="https://github.com/j4ckp0w15g4y/jack-portfolio" target="_blank" rel="noopener noreferrer"> <img src={GitHub} className=""></img></a>                        
-                <a href="https://github.com/j4ckp0w15g4y/show-host" target="_blank" rel="noopener noreferrer"> <img src={GitHub} className=""></img></a>                        
+                {/* <div className="git-link">
 
-                </div>
+                </div> */}
 
 
             </div>
