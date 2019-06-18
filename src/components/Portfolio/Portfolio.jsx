def87b3a8aaf06c81e605d8aa02510fa3a0b691e
Jack Gay <jackpowis@gmail.com>
2019-06-18 17:59:27 -0400

    commit

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -5,6 +5,7 @@ import NYCShooting from "../../Assets/nyc-shooting-incident-hero.png"
 import RecycloHero from "../../Assets/recyclo-hero.png"
 import GitHub from "../../Assets/25231.svg"
 import JackSite from "../../Assets/jack-site.png"
+import ShowHost from "../../Assets/showhost.png"
 
 
 class Portfolio extends Component {
@@ -23,6 +24,7 @@ class Portfolio extends Component {
                    <a href="http://recyclo.surge.sh/" target="_blank" rel="noopener noreferrer"> <img src={RecycloHero} className="project-image"></img></a>
                    {/* <p className="project-info">Recyclo</p>                        */}
                    <a href="https://jackgay.herokuapp.com/" target="_blank" rel="noopener noreferrer"> <img src={JackSite} className="project-image"></img></a>
+                   <a href="https://showhost-nyc.herokuapp.com/" target="_blank" rel="noopener noreferrer"> <img src={ShowHost} className="project-image"></img></a>
 
                 </div>
                 <div className="git-link">
@@ -30,7 +32,8 @@ class Portfolio extends Component {
                 <a href="https://github.com/j4ckp0w15g4y/gun-crime-2018" target="_blank" rel="noopener noreferrer"> <img src={GitHub} className=""></img></a>  
                 <a href="https://github.com/j4ckp0w15g4y/recyclo" target="_blank" rel="noopener noreferrer"> <img src={GitHub} className=""></img></a> 
                 <a href="https://github.com/j4ckp0w15g4y/jack-portfolio" target="_blank" rel="noopener noreferrer"> <img src={GitHub} className=""></img></a>                        
-                       
+                <a href="https://github.com/j4ckp0w15g4y/show-host" target="_blank" rel="noopener noreferrer"> <img src={GitHub} className=""></img></a>                        
+
                 </div>
 
 
