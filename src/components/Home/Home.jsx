d1fd7c681084a399211a54c0d2ff2b32bd808c0e
Jack Gay <jackpowis@gmail.com>
2019-06-25 13:36:26 -0400

    added hero image

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import PortImage from "../../Assets/portfolio-image.jpg";
+import './Home.css'
+
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <h1>I'm a web developer and music publicist currently based in New York, NY.</h1>
+                <img src={PortImage} className="hero-image" title="Jack Gay Image"></img>
             </div>
         );
     }
