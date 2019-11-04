5d1dd2648cf997cb6de6ef9844eae1673ac67435
Jack Gay <jackpowis@gmail.com>
2019-11-04 21:59:02 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
-import { Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import About from './components/About/About'
@@ -15,15 +21,13 @@ function App() {
       <header>
         <Header />
       </header>
-
       <main>
-
-        <Route exact path="/" render={() => <Home />} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/portfolio" component={Portfolio} />
-
-
+        <Switch>
+          <Route exact path="/" render={() => <Home />} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+        </Switch>
       </main>
 
     </div>
